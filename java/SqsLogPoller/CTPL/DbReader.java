9ad4c3ee061dfe96262a027e58b235bde1a7e035
hariharan1101 <98610750+hariharan1101@users.noreply.github.com>
2022-04-19 12:57:43 +0530

    Add files via upload

---
+++ java/SqsLogPoller/CTPL/DbReader.java
@@ -0,0 +1,35 @@
+package SqsLogPoller.CTPL;
+
+import java.io.IOException;
+
+public class DbReader extends Thread{
+	
+	
+	SingletonPoller sp =null;
+	public void run()
+	
+	{
+		while(true)                             //infinite loop that has a 5 second sleep window (adjustable)
+		{
+			sp = MainRunner.sp;      //getting singleton instance
+			if(sp.ReportDetails.get(sp.iterate)!=null)         // if data is available then display data
+			{
+			System.out.println(sp.ReportDetails.get(sp.iterate));
+			sp.iterate++;
+			}
+			else											// else the thread waits for 5 seconds (adjustable) for the DB to be filled with data
+			{
+				try {
+					MainRunner.dr.sleep(5000);
+				} catch (InterruptedException e) {
+		
+					e.printStackTrace();
+				}
+				sp.iterate++;
+			}
+			
+			
+		}
+
+}
+}
